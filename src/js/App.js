05b8043d6d4388bf0bf5c12136bc73a09cd65e3c
m3ltman <m3ltman@gmail.com>
2020-07-06 13:59:44 +0500

    Initialize App.js logic

--- src/js/App.js
+++ src/js/App.js
@@ -1,11 +1,12 @@
-import React, { Component} from "react";
-import {hot} from "react-hot-loader";
+import React, { Component} from 'react';
+import {hot} from 'react-hot-loader';
+import CardList from '../js/components/CardList';
 
 class App extends Component{
   render(){
     return(
       <div className="app">
-        <h1> I made it! </h1>
+        <CardList />
       </div>
     );
   }
