ed6bbe361ab8e30dbe560f4517346ec361ef1160
m3ltman <m3ltman@gmail.com>
2020-10-14 14:19:04 +0500

    Refactor components using Hooks

--- src/js/components/Card.js
+++ src/js/components/Card.js
@@ -1,63 +1,33 @@
-import React, { Component, Fragment } from 'react';
-import handleLike from '../api/handle-like';
+import React, { useEffect, useState } from 'react';
+import changeLike from '../api/change-like';
 import cn from 'classnames';
 
-export default class Card extends Component {
-  constructor(props) {
-    super(props);
+const Card = ({ flat }) => {
+  const [ like, setLike ] = useState(flat.like);
+
+  const likeHanlder = (id) => () => {
+    setLike(!like);
+    changeLike(id, !like);
   }
 
-  like(e, id) {
-    e.persist();
-    let like;
-    if (!e.target.classList.contains('flat-card__like-icon_liked')) {
-      like = true;
-    } else {
-      like = false;
-    }
-    handleLike(id, like)
-      .then((res) => {
-        if(!res.ok) {
-          return Promise.reject(res.status);
-        }
-        return res.json();
-      })
-      .then(_ => e.target.classList.toggle('flat-card__like-icon_liked'))
-      .catch(err => console.log(err));
-  }
+  const likeIconClasses = cn({
+    'flat-card__like-icon': true,
+    'flat-card__like-icon_liked': like ? true : false,
+  });
 
-  createCard() {
-    if (this.props.flatsData) {    
-      const flat = this.props.flatsData.map((flat) => {
-        const likeIconClass = cn({
-          'flat-card__like-icon': true,
-          'flat-card__like-icon_liked': flat.like ? true : false
-         });
-        const address = flat.attributes.address;
-        const agent = flat.relationships.attributes;
-        return(
-          <div className="flat-card" key={flat.id}>
-            <img className="flat-card__image" src={flat.attributes.photo}></img>
-            <div className="flat-card__description">
-              <h3 className="flat-card__title">{flat.attributes.title}</h3>
-              <p className="flat-card__rooms">Количество комнат: {flat.attributes.rooms}</p>
-              <p className="flat-card__address">Адрес: {address.city}, {address.street}-{address.house}, кв.{address.room}</p>
-              <p className="flat-card__area">Площадь: {flat.attributes.area} {flat.attributes.unit}</p>
-              <p className="flat-card__agent">Агент: {agent.last_name} {agent.first_name} {agent.middle_name}</p>
-            </div>
-            <button className={likeIconClass} onClick={(e) => this.like(e, flat.id)}></button>
-          </div>
-        );
-      });
-      return flat;
-    }
-  }
-
-  render() {
-    return(
-      <Fragment>
-        {this.createCard()}
-      </Fragment>
-    )
-  }
+  return (
+    <div className="flat-card">
+      <img className="flat-card__image" src={flat.attributes.photo}></img>
+      <div className="flat-card__description">
+        <h3 className="flat-card__title">{flat.attributes.title}</h3>
+        <p className="flat-card__rooms">Количество комнат: {flat.attributes.rooms}</p>
+        <p className="flat-card__address">Адрес: {flat.attributes.address.city}, {flat.attributes.address.street}-{flat.attributes.address.house}, кв.{flat.attributes.address.room}</p>
+        <p className="flat-card__area">Площадь: {flat.attributes.area} {flat.attributes.unit}</p>
+        <p className="flat-card__agent">Агент: {flat.relationships.attributes.last_name} {flat.relationships.attributes.first_name} {flat.relationships.attributes.middle_name}</p>
+      </div>
+      <button className={likeIconClasses} onClick={likeHanlder(flat.id)}></button>
+    </div>
+  );
 }
+
+export default Card;
--- src/js/components/CardList.js
+++ src/js/components/CardList.js
@@ -1,33 +1,34 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import Card from './Card';
-import getData from '../api/get-data';
+import getFlats from '../api/get-flats';
 import Preloader from './preloader';
 
-export default class CardList extends Component {
-  constructor(props) {
-    super(props);
-    this.state = { isLoading: true };
-  }
+const CardList = () => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [flats, setFlats] = useState([]);
 
-  componentDidMount() {
-    getData()
-      .then((res) => {
-        if (!res.ok) {
-          return Promise.reject(res.status);
-        }
-        return res.json();
-      })
-      .then((flatsData) => {
-        this.setState({ flatsData: flatsData, isLoading: false });
+  useEffect (() => {
+    getFlats()
+      .then(flats => {
+        setFlats(flats);
+        setIsLoading(false);
       })
       .catch(err => console.log(err));
-  }
+  }, []);
 
-  render() {
-    return(
-      <section className="cards">
-        {this.state.isLoading ? <Preloader /> : <Card flatsData={this.state.flatsData} />}
-      </section>
-    );
-  }
-}
+  return (
+    <>
+      {flats.length > 0 &&
+        <section className="cards" style={isLoading ? {display: 'flex'} : {display:'grid'}}>
+          {isLoading ? 
+            <Preloader />
+            :
+            flats.map(flat => <Card flat={flat} key={flat.id}/>)
+          }
+        </section>
+      }
+    </>
+  );
+};
+
+export default CardList;
